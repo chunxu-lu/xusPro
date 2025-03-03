04b00874f5351f12f1139bc079266206ff47e734
lcx227 <2274606915@qq.com>
2025-03-03 21:13:07 +0800

    1

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="page-container">
-    <div class="switch"><el-switch v-model="useThemeStore().theme" /></div>
+    <div class="switch">
+      <el-switch v-model="useThemeStore().theme" />
+    </div>
     <div class="login-wrap">
       <el-form ref="ruleFormRef" :model="ruleForm" size="large">
         <Motion :delay="100">
@@ -16,52 +18,71 @@
         </Motion>
       </el-form>
       <el-button class="login-btn" type="primary" @click="login"> 登录 </el-button>
+      <Vcode :show="isShow" @success="onSuccess" @close="onClose" successText="哈哈"/>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { useThemeStore } from '@/pinia/index'
-import { doLogin, getUserInfo } from '@/api/login'
-import router from '@/router'
-import { User, Lock } from '@element-plus/icons-vue'
-const ruleForm = reactive({
-  username: 'user',
-  password: 'password'
-})
-watch(
-  () => useThemeStore().theme,
-  () => {
-    useThemeStore().toggleTheme()
-  }
-)
+  import { useThemeStore } from '@/pinia/index'
+  import { doLogin, getUserInfo } from '@/api/login'
+  import router from '@/router'
+  import { User, Lock } from '@element-plus/icons-vue'
+  import { debounce, _ } from 'lodash'
+  import Vcode from 'vue3-puzzle-vcode'
 
-async function login() {
-  const res = await doLogin(ruleForm)
-  sessionStorage.setItem('token', res.jwtToken)
-  await getUserInfo()
-  router.push('/home')
-}
+  console.log(_.uniq([1, 1, 2, 3]))
+
+  const isShow = ref(true)
+
+  function onSuccess(){
+
+  }
+
+  function onClose(){
+
+  }
+
+  const ruleForm = reactive({
+    username: 'user',
+    password: 'password'
+  })
+  watch(
+    () => useThemeStore().theme,
+    () => {
+      useThemeStore().toggleTheme()
+    }
+  )
+
+  async function login() {
+    const res = await doLogin(ruleForm)
+    sessionStorage.setItem('token', res.jwtToken)
+    await getUserInfo()
+    router.push('/home')
+  }
 </script>
 
 <style lang="scss" scoped>
-.page-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  position: relative;
-  .switch {
-    position: absolute;
-    top: 50px;
-    right: 80px;
-  }
-}
-.login-wrap {
-  width: 300px;
-  .login-btn {
+  .page-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 100%;
+    height: 100%;
+    position: relative;
+
+    .switch {
+      position: absolute;
+      top: 50px;
+      right: 80px;
+    }
   }
-}
+
+  .login-wrap {
+    width: 300px;
+
+    .login-btn {
+      width: 100%;
+    }
+  }
 </style>
